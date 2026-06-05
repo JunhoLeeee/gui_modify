ea4b504feb9a916f7200e9f9e35facb07c16bdb5
JunhoLeeee <rush2901@naver.com>
2026-06-05 15:28:32 +0900

    Refactor GUI architecture and service structure7

--- BroadcastControl.App/ViewModels/Motor/MotorControlViewModel.cs
+++ BroadcastControl.App/ViewModels/Motor/MotorControlViewModel.cs
@@ -288,11 +288,6 @@ public sealed partial class MainViewModel
             return;
         }
 
-        if (buttons == MotorButtonMask.None)
-        {
-            return;
-        }
-
         ApplyMotorButtonStateToCommandTarget(buttons);
 
         var sendMask = (buttons & MotorButtonMask.Center) != 0 ? MotorButtonMask.None : buttons;
